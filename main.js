e92c75aa6cdcabac06bf27890f026361d9390810
Gennady.Sharanov <ge.u1@yandex.ru>
2020-08-07 11:16:56 +0400

    fix bug

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ function getFiles(setFiles = 0){
 	//
 	let progressBar = document.getElementById("progressBar");
 	let barText = document.getElementById("barText");
-	//
 	//console.log(img.length);
 	let onLoadedFiles = 0;
 	//
@@ -23,7 +22,7 @@ function getFiles(setFiles = 0){
 	for (let i = 0; files.length > i; i++) {
 		filesSize += files[i].size;
 	}
-	console.log(filesSize + " - size files");
+	//console.log(filesSize + " - size files");
 	//
 	let reader =  new FileReader();
 	//console.log(files[0])
@@ -33,7 +32,7 @@ function getFiles(setFiles = 0){
 	reader.readAsDataURL(files[iteration]);
 	reader.onloadend = function(event){
 		onLoadedFiles += files[iteration].size;
-		console.log("size file = " + onLoadedFiles)
+		//console.log("size file = " + onLoadedFiles)
 		//create preview picture
 		if(countFilesAll < img.length){
 			img[countFilesAll].src = reader.result;
@@ -53,9 +52,9 @@ function getFiles(setFiles = 0){
 		}
 	}
 	reader.onprogress = function(event){
-		console.log(event.total);
-
-		let progress = (onLoadedFiles + event.total)/filesSize*100;
+		//console.log(event.total);
+		console.log(event.loaded);
+		let progress = (onLoadedFiles + event.loaded)/filesSize*100;
 		barText.innerHTML = Math.round(progress) + " %";
 		progressBar.value = progress;
 	}
@@ -88,33 +87,6 @@ function handleDrop(event) {
 	dropArea.style.backgroundColor = "#f1f1f1"
 	getFiles(files);
 }
-
-
-/*
-let pictureBox = document.getElementsByClassName("picture page__picture")[0];
-let firstFile,secondFile;
-
-pictureBox.onmousedown = function(event){
-	let element = event.target.closest('div');
-	if(element.className === "picture-box picture__container"){
-		if(element.firstChild.nextElementSibling.currentSrc){
-			firstFile= element
-		}
-	}
-}
-
-pictureBox.onmouseup = function(event){
-	let element = event.target.closest('div');
-	if(element.className === "picture-box picture__container"){
-		if(element.firstChild.nextElementSibling.currentSrc){
-			secondFile = element;
-			//console.log(element);
-			let c = firstFile.cloneNode(true)
-			c = secondFile.parentNode.replaceChild(c, secondFile);
-			firstFile.parentNode.replaceChild(c, firstFile);
-		}
-	}
-}*/
 
 let pictureBox = document.getElementsByClassName("picture page__picture")[0];
 let firstFile,secondFile;
